a29ecafe5d7c0d4a53c9c60ac6e01b01b9c49a1f
nayeem43 <nayeemahmedru@gmail.com>
2023-03-23 23:46:21 +0600

    db connection and routing

--- starter/app.js
+++ starter/app.js
@@ -4,17 +4,22 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
 //middleware
 app.use(express.json());
 
-//roots
+//routes
 
 app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
+
+app.use('/api/v1/products', productsRouter);
 
 //products route
 
@@ -26,6 +31,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     // connect DB
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening port ${port}...`));
   } catch (error) {
     console.log(error);
